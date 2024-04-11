19ec0d5706adfb3aaf3635413d4da8c953535534
Bagrat Ashibokov <bagrat_ashba@mail.ru>
2024-04-11 18:17:44 +0300

    watch

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
+import Row from 'react-bootstrap/Row';
 import AdditionForm from '../ui/AdditionForm';
+import OneWatchCard from '../ui/OneWatchCard';
+import AppCorousel from '../ui/AppCorousel';
 
-export default function MainPage({ ress }) {
-  const [rs, setRes] = useState(ress);
-
+export default function MainPage({ watch, user, request }) {
+  //   const { currentPosts, submitHandler, deleteHandler } = usePosts({ watch });
+  const [rs, setRes] = useState(request);
   return (
-    <AdditionForm setRes={setRes} />
+    <>
+      <Row>
+        <AppCorousel />
+      </Row>
+      <Row>
+        {watch?.map((el) => <OneWatchCard key={el.id} watch={el} user={user} />)}
+      </Row>
+      <AdditionForm setRes={setRes} />
+    </>
   );
 }
--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-
 export default function AppNavbar() {
   // const { signUpHandler, logoutHandler } = useAuth();
   return (
